263125afc353639e382bb7f87b7e642daa64d782
Jack Branthwaite <jackb@bikematrix.io>
2025-04-08 14:19:08 +1200

    Links added, small footer fix

--- src/theme/Footer/Branding/index.tsx
+++ src/theme/Footer/Branding/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./styles.module.css";
+import Link from "@docusaurus/Link";
 
 export const Branding = () => {
   return (
@@ -9,21 +10,27 @@ export const Branding = () => {
           <img src={require("@site/static/img/white-logo.png").default} />
         </div>
         <div className={s.socials}>
-          <img
-            src={require("@site/static/img/linked.png").default}
-            alt="LinkedIn Logo"
-            className={s.logo}
-          />
-          <img
-            src={require("@site/static/img/fb.png").default}
-            alt="Facebook Logo"
-            className={s.logo}
-          />
-          <img
-            src={require("@site/static/img/insta.png").default}
-            alt="Instagram Logo Logo"
-            className={s.logo}
-          />
+          <Link href="https://www.linkedin.com/company/bike-matrix-ltd/">
+            <img
+              src={require("@site/static/img/linked.png").default}
+              alt="LinkedIn Logo"
+              className={s.logo}
+            />
+          </Link>
+          <Link href="https://www.facebook.com/BikeMatrixLtd">
+            <img
+              src={require("@site/static/img/fb.png").default}
+              alt="Facebook Logo"
+              className={s.logo}
+            />
+          </Link>
+          <Link href="https://www.instagram.com/bike_matrix/">
+            <img
+              src={require("@site/static/img/insta.png").default}
+              alt="Instagram Logo Logo"
+              className={s.logo}
+            />
+          </Link>
         </div>
       </div>
       <div className={s.copyright}>Copyright Bike Matrix 2025</div>
